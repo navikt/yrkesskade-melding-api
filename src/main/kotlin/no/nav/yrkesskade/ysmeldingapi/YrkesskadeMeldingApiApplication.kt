5ff16b05d18dd7b2aefe0ed1edbc2baa9cbc73a4
Erlend Kristiansen <erlend.kristiansen@nav.no>
2021-09-30 13:17:53 +0200

    Initial commit

---
+++ src/main/kotlin/no/nav/yrkesskade/ysmeldingapi/YrkesskadeMeldingApiApplication.kt
@@ -0,0 +1,11 @@
+package no.nav.yrkesskade.ysmeldingapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YrkesskadeMeldingApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<YrkesskadeMeldingApiApplication>(*args)
+}
---
+++ src/test/kotlin/no/nav/yrkesskade/ysmeldingapi/YrkesskadeMeldingApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.nav.yrkesskade.ysmeldingapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YrkesskadeMeldingApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
