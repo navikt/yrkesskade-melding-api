341d98568076e1ca925450a9ccaf3890fc298001
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2021-10-04 17:06:44 +0200

    Made SkademeldingDto a data class

--- src/main/kotlin/no/nav/yrkesskade/ysmeldingapi/models/SkademeldingDto.kt
+++ src/main/kotlin/no/nav/yrkesskade/ysmeldingapi/models/SkademeldingDto.kt
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.yrkesskade.ysmeldingapi.domain.Skademelding
 import java.time.Instant
 
-class SkademeldingDto(val tekst: String, val nummer: Int, val bool: Boolean, val dato: Instant?) {
+data class SkademeldingDto(val tekst: String, val nummer: Int, val bool: Boolean, val dato: Instant?) {
 
     fun toSkademelding(): Skademelding {
         return Skademelding(
