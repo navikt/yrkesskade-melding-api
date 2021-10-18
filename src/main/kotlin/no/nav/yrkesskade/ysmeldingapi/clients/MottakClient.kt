f3d41a57d84b8cb59e39a1462df17047f85c19c1
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2021-10-18 15:15:36 +0200

    MottakClient: * Spesifiserer mediatype i requestet * Bruker okhttp sin string() istedenfor Kotlin sin toString()

--- src/main/kotlin/no/nav/yrkesskade/ysmeldingapi/clients/MottakClient.kt
+++ src/main/kotlin/no/nav/yrkesskade/ysmeldingapi/clients/MottakClient.kt
@@ -3,10 +3,12 @@ package no.nav.yrkesskade.ysmeldingapi.clients
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.yrkesskade.ysmeldingapi.models.SkademeldingDto
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 
 @Component
@@ -18,12 +20,12 @@ class MottakClient(@Value("\${api.client.yrkesskade-mottak-url}") private val mo
         val postBody = jacksonObjectMapper().writeValueAsString(skademelding)
         val request = Request.Builder()
                 .url("$mottakBaseUrl/api/skademelding/")
-                .post(postBody.toRequestBody())
+                .post(postBody.toRequestBody(MediaType.APPLICATION_JSON_VALUE.toMediaType()))
                 .build()
 
         client.newCall(request).execute().use { response ->
             if (!response.isSuccessful) throw RuntimeException("Unexpected code $response")
-            return jacksonObjectMapper().readValue(response.body.toString())
+            return jacksonObjectMapper().readValue(response.body!!.string())
         }
     }
 }
