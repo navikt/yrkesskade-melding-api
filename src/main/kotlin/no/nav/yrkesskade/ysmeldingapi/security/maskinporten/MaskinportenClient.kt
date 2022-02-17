9c092ce40ec8ac89fc45de31fe10fb9aba5f7724
Jone Lura <jone.lura@nav.no>
2022-02-17 12:31:36 +0100

    YSMOD-58
    - gi mer utfyllende melding ved feil

--- src/main/kotlin/no/nav/yrkesskade/ysmeldingapi/security/maskinporten/MaskinportenClient.kt
+++ src/main/kotlin/no/nav/yrkesskade/ysmeldingapi/security/maskinporten/MaskinportenClient.kt
@@ -47,7 +47,7 @@ class MaskinportenClientImpl(
             .issueTime(Date.from(now))
             .expirationTime(Date.from(expire))
             .notBeforeTime(Date.from(now))
-            .claim("scope", "altinn:serviceowner/reportees altinn:serviceowner/rolesandrights")
+            .claim("scope", "altinn:serviceowner")
             .claim("resource", altinnUrl)
             .jwtID(UUID.randomUUID().toString())
             .build()
