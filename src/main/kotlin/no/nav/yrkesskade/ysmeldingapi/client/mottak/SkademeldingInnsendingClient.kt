3de47c5a209d3c4a838db57a1c291ab500976639
Jone Lura <jone.lura@nav.no>
2022-03-07 07:17:13 +0100

    YSMOD-76
    - endret til JSON serializer
    - satte på client-id på producer konfigurasjon
    - opprettet kafka producerfactory og template for skademelding

--- src/main/kotlin/no/nav/yrkesskade/ysmeldingapi/client/mottak/SkademeldingInnsendingClient.kt
+++ src/main/kotlin/no/nav/yrkesskade/ysmeldingapi/client/mottak/SkademeldingInnsendingClient.kt
@@ -5,19 +5,20 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.yrkesskade.model.SkademeldingInnsendtHendelse
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.SendResult
 import org.springframework.stereotype.Component
+import org.springframework.util.concurrent.ListenableFuture
 
 @Component
 class SkademeldingInnsendingClient(
     @Value("\${kafka.topic.skademelding-innsendt}") private val mottakTopic: String,
-    private val kafkaTemplate: KafkaTemplate<String, String>
+    private val skademeldingKafkaTemplate: KafkaTemplate<String, SkademeldingInnsendtHendelse>
 ) {
     val objectMapper = jacksonObjectMapper().registerModule(JavaTimeModule())
 
     fun sendTilMottak(skademelding: SkademeldingInnsendtHendelse): SkademeldingInnsendtHendelse {
-        val skademeldingString = objectMapper.writeValueAsString(skademelding)
-        val resultat = kafkaTemplate.send(mottakTopic, skademeldingString).get()
-
-        return objectMapper.readValue(resultat.producerRecord.value(), SkademeldingInnsendtHendelse::class.java)
+        val future: ListenableFuture<SendResult<String, SkademeldingInnsendtHendelse>> = skademeldingKafkaTemplate.send(mottakTopic, skademelding)
+        val resultat = future.get()
+        return resultat.producerRecord.value()
     }
 }
