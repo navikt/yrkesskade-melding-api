3ee5f249b62634114f39552fac77c122fa0950e8
Jone Lura <jone.lura@gmail.com>
2022-09-28 10:12:11 +0200

    YSMOD-623
    lagt til støtte for ulike institusjonsrollene
    - lagt til testdata

--- src/main/kotlin/no/nav/yrkesskade/ysmeldingapi/models/Skjematype.kt
+++ src/main/kotlin/no/nav/yrkesskade/ysmeldingapi/models/Skjematype.kt
@@ -6,7 +6,11 @@ enum class Skjematype(
     LAERLING("laerling"),
     ELEV_ELLER_STUDENT("elevEllerStudent"),
     TILTAKSDELTAKER("tiltaksdeltaker"),
-    VERNEPLIKTIG_I_FOERSTEGANGSTJENESTEN("vernepliktigIFoerstegangstjenesten");
+    VERNEPLIKTIG_I_FOERSTEGANGSTJENESTEN("vernepliktigIFoerstegangstjenesten"),
+    INNSATT("innsatt"),
+    PERSON_SOM_UTFOERER_SAMFUNNSSTRAFF("personSomUtfoererSamfunnsstraff"),
+    PERSON_I_VARETEKT("personIVaretekt")
+    ;
 
     companion object {
         private val map = Skjematype.values().associateBy(Skjematype::rolletype)
